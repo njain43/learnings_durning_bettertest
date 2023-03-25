4843ae6b697e3c5175af243a0d9b30d8571e4e48
Nitesh Jain <mailtoniteshjain@gmail.com>
2023-03-26 00:02:04 +0530

    1) test with different msg file 2) naming fixes

--- src/converter_indexer/indexer.py
+++ src/converter_indexer/indexer.py
@@ -4,13 +4,18 @@ from src.converter_indexer import samples
 from pathlib import Path
 
 
-class indexer:
+class Indexer:
 
-    def __init__(self, fields_path: str = None, sample_order_path: str = None):
-        self._fields_file = (Path(samples.__file__).parent / 'fields.json').read_text()
-        self._order_file = (Path(samples.__file__).parent / 'order.json').read_text()
+    def __init__(self, fields_data: str = None, msg_data: str = None):
+
+        # default fileds and msg files
+        self._fields_file = (
+                    Path(samples.__file__).parent / 'fields.json').read_text() if fields_data is None else fields_data
+        self._order_file = (Path(
+            samples.__file__).parent / 'order.json').read_text() if msg_data is None else msg_data
+
         self._fields = json.loads(self._fields_file)
-        self._order = json.loads(self._order_file)
+        self._msg = json.loads(self._order_file)
 
     def __str__(self):
         print(self.fn2ft2fd())
@@ -20,7 +25,7 @@ class indexer:
         for value in self._fields['message_definition']:
             f_name = value['name']
             f_type = value['type']
-            f_value = self._order[f_name]
+            f_value = self._msg[f_name]
             fn2ft2fd.update({f_name: {"f_name": f_name, 'f_type': f_type, 'f_value': f_value}})
 
         return fn2ft2fd
--- tests/unit/converter_indexer/test_indexer.py
+++ tests/unit/converter_indexer/test_indexer.py
@@ -1,12 +1,23 @@
 from unittest import TestCase
-from src.converter_indexer.indexer import indexer
+from src.converter_indexer.indexer import Indexer
+from src.converter_indexer import samples
+from pathlib import Path
 
 
 class TestIndexer(TestCase):
 
     def setUp(self) -> None:
-        self._indx = indexer().fn2ft2fd()
+        self._indx = Indexer().fn2ft2fd()
 
     def test_indexer(self):
         self.assertEqual('NewOrderSingle', self._indx['MsgType'].get('f_value'))
         self.assertEqual(21, self._indx['Quantity'].get('f_value'))
+
+    def test_indexer_with_another_msg(self):
+        self._order_file = (Path(
+            samples.__file__).parent / 'ExecutionReport.json').read_text()
+        self._indx = Indexer(msg_data=self._order_file).fn2ft2fd()
+        self.assertEqual('ExecutionReport', self._indx['MsgType'].get('f_value'))
+
+
+
