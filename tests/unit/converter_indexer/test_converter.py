663b26e9db0ced626c47f9ac9378e02ce82e893e
Nitesh Jain <mailtoniteshjain@gmail.com>
2023-03-29 10:17:32 +0530

    byte stream to json

--- tests/unit/converter_indexer/test_converter.py
+++ tests/unit/converter_indexer/test_converter.py
@@ -17,10 +17,10 @@ class TestIndexer(TestCase):
 
     def test_jsn2bytes(self):
         output = self._conv.json2bytes()
-        print(f'bytes- {output}')
+        # print(f'bytes- {output}')
         self.assertEqual([b'NewOrderSingle', b'\x15\x00', b'INFY', b'fffff\x84\x94@', b'Nitesh', b'B', b'IOC', b'NSE'], output)
 
     def test_bytes2json(self):
         jsn = self._conv.bytes2json(self._conv.json2bytes())
-        print(f'jsn - {jsn}')
+        # print(f'jsn - {jsn}')
         self.assertDictEqual(jsn, self.expected_json)
