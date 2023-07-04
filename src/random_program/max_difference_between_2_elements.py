20e6b68182da25f2ff539692fd3f55c8b060ccff
Nitesh Jain <mailtoniteshjain@gmail.com>
2023-07-04 23:20:19 +0530

    max profit from share price list and its unit test

---
+++ src/random_program/max_difference_between_2_elements.py
@@ -0,0 +1,46 @@
+"""
+A list has price of a security for 10 differet days, starting from day1 to day 10.
+Find out the 2 days for buy/sell to generate max profit.
+
+"""
+
+
+class MinMax:
+
+    def __init__(self, l: list):
+        self.price_list = l
+
+    def get_max_value_from_list(self):
+        return max(self.price_list)
+
+    def get_min_value_from_list(self):
+        return min(self.price_list)
+
+    def get_profit(self):
+        return self.get_max_value_from_list() - self.get_min_value_from_list()
+
+
+class MaxPossibleProfit:
+
+    def __init__(self, list_of_price):
+        self.list_of_price = list_of_price
+
+    def get_indx(self, price: int | float = None) -> int:
+        if price is None:
+            price = self.list_of_price[0]
+        return self.list_of_price.index(price)
+
+    def get_profit(self) -> int | float:
+        indx_buy_price = self.get_indx()
+        indx_sell_price = None
+        buy_price = self.list_of_price[0]
+        sell_price = self.list_of_price[0]
+        for i in self.list_of_price:
+            if i < buy_price and (indx_sell_price is None or self.get_indx(i) > indx_sell_price):
+                buy_price = i
+                indx_buy_price = self.get_indx(buy_price)
+            elif i > sell_price and self.get_indx(i) < indx_buy_price:
+                sell_price = i
+                indx_sell_price = self.get_indx(sell_price)
+
+        return sell_price - buy_price
---
+++ tests/unit/random/share_price_profit.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from src.random_program.max_difference_between_2_elements import MinMax, MaxPossibleProfit
+
+
+class TestMinMax(TestCase):
+
+    def setUp(self) -> None:
+        sample_price_list = [1, 2, 4, 3, 8, 10, 9]
+        share_price_list = [1, 4, .5, 2, 3, 7, 5, 8, 6, 8]
+        self.obj = MinMax(sample_price_list)
+        self.max_profit = MaxPossibleProfit(share_price_list)
+
+    def test_get_max_value(self):
+        self.assertEqual(10, self.obj.get_max_value_from_list())
+
+    def test_get_min_value(self):
+        self.assertEqual(1, self.obj.get_min_value_from_list())
+
+    def test_profit(self):
+        self.assertEqual(9, self.obj.get_profit())
+
+    def test_max_profit(self):
+        self.assertEqual(7.5, self.max_profit.get_profit())
